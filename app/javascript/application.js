6a0212377a913b8ebf8091a5c8da34d4e309324b
Benny Cheung <daydreamer10191980@gmail.com>
2022-01-19 15:18:25 +0800

    inital commit

---
+++ app/javascript/application.js
@@ -0,0 +1,10 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+//*** I added the following 4 lines to make JQuery work ***
+
+//= require jquery
+//= require jquery_ujs
+// require turbolinkss
+//= require_tree .
